52291e3e85e5c90ba6047437ddb3112d358b1200
tromili <anthonny.gonzales@gmail.com>
2017-04-06 09:42:07 -0500

    avances

---
+++ ejercicios.c
@@ -0,0 +1,6 @@
+/*1)
+	1.1)  usamos strlen(var) + 1 ya que al sacar strlen sacamos la longitud del array pero no agarramos el ultimo elemento.
+	1.2) si agarramos el MAX_LENGTH , agarraremos basura, mientras con el strlen agarramos la cantidad de caracteres que necesitamos.
+2)
+*/	
+
--- procesos.c
+++ procesos.c
@@ -6,7 +6,7 @@ const int MAX_STRING = 100;
 
 int main(void){
 
-	char greeting[MAX_STRING];
+	//char greeting[MAX_STRING];
 	int comm_sz;
 	int my_rank;
 	int ball = 0;
@@ -14,20 +14,34 @@ int main(void){
 	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
+	if(ball==0 && my_rank ==0)
+		if(ball <10){
+			printf("\nProceso 0:");
+			printf("%d",ball);
+			ball++;	
+			MPI_Send(&ball,1,MPI_INT,1,0,MPI_COMM_WORLD);
+		}//else break;
 	while (ball < 5)
 	{
 
-		if(ball==0){
-			if(ball <10){
-				ball++;
-				MPI_Send(ball,1,MPI_INT,0,0,MPI_COMM_WORLD);
-			}else break;
+		
+		if(my_rank==1){
+			MPI_Recv(&ball,1,MPI_INT,0,0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+			printf("\nProceso 1:");
+			printf("%d",ball);
+			printf("\n");
+			//ball = ballc;
+			ball++;
+			MPI_Send(&ball,1,MPI_INT,0,0,MPI_COMM_WORLD);
 		}else{
-			if(ball<=10){
-				MPI_Recv(ball,1,MPI_INT,1,0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-				ball++;
-			}else break;
-		}
+			MPI_Recv(&ball,1,MPI_INT,1,0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+			printf("\nProceso 0:");
+			printf("%d",ball);
+			//ball = ballc;
+			ball++;
+			MPI_Send(&ball,1,MPI_INT,1,0,MPI_COMM_WORLD);
+		}//else break;
+		
 		/* code */
 	}
 }
